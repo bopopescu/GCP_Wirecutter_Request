1790af6f030c6f0ffe4e62aeafea7e3ca7166c10
Reiniel Ravelo <reinielravelo@gmail.com>
2019-08-13 17:00:38 -0400

    update

--- GCP_App_Engine/python-docs-samples/appengine/standard_python37/hello_world/main.py
+++ GCP_App_Engine/python-docs-samples/appengine/standard_python37/hello_world/main.py
@@ -1,31 +1,59 @@
-from google.appengine.api import urlfetch
+  
+# Copyright 2016 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-try:
-   form_data = urllib.urlencode(UrlPostHandler.form_fields)
-   headers = {
-        'X-Api-Key': 'xmQsE6BowZel9xM5hDyFr4PI',
-    },
-     auth : {
-       'username': 'nytimes',
-       'password': 'EWp9rsWSB4pL28'
-   }
-   result = urlfetch.fetch(
-       url='https://phoenix.submarineleisureclub.com/api/1.0/sources/1322',
-       payload=form_data,
-       method=urlfetch.GET,
-       headers=headers)
-   self.response.write(result.content)
-except urlfetch.Error:
-   logging.exception('Caught exception fetching url')
+# [START app]
+import logging
+
+from flask import Flask
+
+# [START imports]
+import requests
+import requests_toolbelt.adapters.appengine
+
+# Use the App Engine Requests adapter. This makes sure that Requests uses
+# URLFetch.
+requests_toolbelt.adapters.appengine.monkeypatch()
+# [END imports]
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    # [START requests_get]
+    url = 'https://storage.googleapis.com/nyt-ads-static-assets/ads/adplatforms/HD_Source_150080.json'
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.text
+    # [END requests_get]
+
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
+# [END app]
 
 
 
-# url = 'https://api.thedogapi.com/v1/breeds'
-# try:
-#     result = urlfetch.fetch(url)
-#     if result.status_code == 200:
-#         self.response.write(result.content)
-#     else:
-#         self.response.status_code = result.status_code
-# except urlfetch.Error:
-#     logging.exception('Caught exception fetching url')
+
+# Error - file: 'file:///Users/208414/Desktop/GithubRepos/GCP_Wirecutter_Request/GCP_App_Engine/python-docs-samples/appengine/standard_python37/hello_world/main.py'
+# severity: 'Error'
+# message: 'E0401:Unable to import 'requests_toolbelt.adapters.appengine''
+# at: '23,1'
+# source: 'pylint'
