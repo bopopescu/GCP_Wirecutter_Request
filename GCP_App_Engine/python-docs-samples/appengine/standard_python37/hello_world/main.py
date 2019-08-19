d3caef6ad9ab408710b0c3c4fc3573ba1e61024b
Reiniel Ravelo <reinielravelo@gmail.com>
2019-08-19 10:31:47 -0400

    update

--- GCP_App_Engine/python-docs-samples/appengine/standard_python37/hello_world/main.py
+++ GCP_App_Engine/python-docs-samples/appengine/standard_python37/hello_world/main.py
@@ -3,9 +3,14 @@ import logging
 from urllib3 import PoolManager
 from urllib3.contrib.appengine import AppEngineManager, is_appengine_sandbox
 from flask import Flask
-
+# imports to read and write to buckets 
+import os
+import cloudstorage as gcs
+import webapp2
+# to make requests 
 import requests
 import requests_toolbelt.adapters.appengine
+from google.appengine.api import app_identity
 
 # Use the App Engine Requests adapter. This makes sure that Requests uses
 # URLFetch.
@@ -36,3 +41,44 @@ def server_error(e):
     An internal error occurred: <pre>{}</pre>
     See logs for full stacktrace.
     """.format(e), 500
+
+
+
+# code to send to buckets 
+# Specifying the Cloud Storage bucket
+
+def get(self):
+      bucket_name = os.environ.get('BUCKET_NAME',
+                               app_identity.get_default_gcs_bucket_name())
+
+  self.response.headers['Content-Type'] = 'text/plain'
+  self.response.write('Demo GCS Application running from Version: '
+                      + os.environ['CURRENT_VERSION_ID'] + '\n')
+  self.response.write('Using bucket name: ' + bucket_name + '\n\n')
+
+
+# Writing to Cloud Storage
+def create_file(self, filename):
+      """Create a file.
+
+  The retry_params specified in the open call will override the default
+  retry params for this particular file handle.
+
+  Args:
+    filename: filename.
+  """
+  self.response.write('Creating file %s\n' % filename)
+
+  write_retry_params = gcs.RetryParams(backoff_factor=1.1)
+  gcs_file = gcs.open(filename,
+                      'w',
+                      content_type='text/plain',
+                      options={'x-goog-meta-foo': 'foo',
+                               'x-goog-meta-bar': 'bar'},
+                      retry_params=write_retry_params)
+  gcs_file.write('abcde\n')
+  gcs_file.write('f'*1024*4 + '\n')
+  gcs_file.close()
+  self.tmp_filenames_to_clean_up.append(filename)
+
+
